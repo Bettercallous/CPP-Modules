c2800be728effed6cb2d99de004628a3589072ab
bettercallous <bettercallous@gmail.com>
2023-11-20 09:46:21 +0100

    ...

--- cpp_01/ex05/main.cpp
+++ cpp_01/ex05/main.cpp
@@ -7,5 +7,16 @@ int main()
 	std::cout << "Enter level: ";
 	getline(std::cin, level);
 	harl.complain(level);
+	// tests //
+	std::cout << "------- Testing -------" << std::endl;
+	std::cout << "DEBUG level: " << std::endl;
+	harl.complain("DEBUG");
+	std::cout << "INFO level: " << std::endl;
+	harl.complain("INFO");
+	std::cout << "WARNING level: " << std::endl;
+	harl.complain("WARNING");
+	std::cout << "ERROR level: " << std::endl;
+	harl.complain("ERROR");
+	std::cout << "------------------------" << std::endl;
 	return 0;
 }
