952cb16dca2fa6309d0c1d1d26ae23f13f432f79
Oussama Belhaj <oubelhaj@e2r1p12.1337.ma>
2023-12-01 23:53:41 +0100

    ...

--- cpp_03/ex00/ClapTrap.cpp
+++ cpp_03/ex00/ClapTrap.cpp
@@ -1,8 +1,11 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {}
+ClapTrap::ClapTrap() {
+	std::cout << "Default constructor called" << std::endl;
+}
 
 ClapTrap::ClapTrap(const std::string name) {
+	std::cout << "Constructor called" << std::endl;
 	this->name = name;
 	this->hitPoints = 10;
 	this->energyPoints = 10;
@@ -10,6 +13,7 @@ ClapTrap::ClapTrap(const std::string name) {
 }
 
 ClapTrap::ClapTrap(const ClapTrap& other) {
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
@@ -17,16 +21,35 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
 	this->name = other.name;
 	this->attackDamage = other.attackDamage;
 	this->hitPoints = other.hitPoints;
+	return *this;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called" << std::endl;
 }
 
 void ClapTrap::attack(const std::string& target) {
-	
+	if (energyPoints > 0) {
+		std::cout << "ClapTrap " << name << " attacks " << target << " causing "
+				  << attackDamage <<  " points of damage !" << std::endl;
+		takeDamage(attackDamage);
+	}
+	else
+		std::cout << "ClapTrap " << name << " has no energy left" << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
-
+	hitPoints -= amount;
+	energyPoints--;
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-
+	if (!energyPoints)
+		std::cout << "ClapTrap " << name << " has no energy left" << std::endl;
+	else {
+		std::cout << "ClapTrap " << name << " repaired " << amount
+				  << " points of energy !" << std::endl;
+		hitPoints += amount;
+		energyPoints--;
+	}
 }
---
+++ cpp_03/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	// ClapTrap a("alaoui");
+
+	// a.attack("alaoui2");
+	// a.beRepaired(2);
+}
