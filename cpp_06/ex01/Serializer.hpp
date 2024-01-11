de5c917565da8fbe16606052e37e86fed3168477
bettercallous <bettercallous@gmail.com>
2024-01-11 01:33:29 +0100

    ...

--- cpp_06/ex01/Serializer.hpp
+++ cpp_06/ex01/Serializer.hpp
@@ -3,15 +3,14 @@
 
 #include <iostream>
 #include "Data.hpp"
-#include <cstdint>
 
 class Serializer {
     private:
         Serializer();
-    public:
         Serializer(const Serializer & other);
         Serializer& operator=(const Serializer & other);
         ~Serializer();
+    public:
 
         static uintptr_t serialize(Data* ptr);
         static Data* deserialize(uintptr_t raw);
