06aec7d9e4b2d0d80cef619738e2ab0bd4bc7d74
bettercallous <bettercallous@gmail.com>
2024-01-31 19:08:17 +0100

    ...

--- Module_09/ex00/BitcoinExchange.cpp
+++ Module_09/ex00/BitcoinExchange.cpp
@@ -104,53 +104,53 @@ std::pair<bool, std::pair<std::string, double> > BitcoinExchange::parseInput(con
     }
 
     double value;
-    if (checkValue(value, val) == -1) {
+    if (checkValue(value, val) == false) {
         return std::make_pair(false, std::make_pair("", 0));
     }
 
-    if (checkDate(key) == -1) {
+    if (checkDate(key) == false) {
         return std::make_pair(false, std::make_pair("", 0));
     }
     
     return std::make_pair(true, std::make_pair(key, value));
 }
 
-int BitcoinExchange::checkValue(double& value, const std::string& valueStr) {
+bool BitcoinExchange::checkValue(double& value, const std::string& valueStr) {
     std::istringstream buf(valueStr);
 
     if (isNumeric(valueStr, ".0123456789") == false) {
         std::cerr << "Error: invalid number => " << buf.str() << std::endl;
-        return -1;
+        return false;
     }
 
     // Check for multiple decimal points
     if (std::count(valueStr.begin(), valueStr.end(), '.') > 1) {
         std::cerr << "Error: invalid number => " << buf.str() << std::endl;
-        return -1;
+        return false;
     }
 
     // Check if the string starts with a decimal point
     if (valueStr[0] == '.') {
         std::cerr << "Error: invalid number => " << buf.str() << std::endl;
-        return -1;
+        return false;
     }
 
     // Check if the string ends with a decimal point
     if (valueStr[valueStr.length() - 1] == '.') {
         std::cerr << "Error: invalid number => " << buf.str() << std::endl;
-        return -1;
+        return false;
     }
     buf >> value;
     // cant be negative because of isNumeric function above
     if (value > 1000) {
         std::cerr << "Error: number too large." << std::endl;
-        return -1;
+        return false;
     }
 
-    return 0;
+    return true;
 }
 
-int BitcoinExchange::checkDate(const std::string& date) {
+bool BitcoinExchange::checkDate(const std::string& date) {
     std::string tokens[4];
     std::istringstream iss(date);
     std::string token;
@@ -162,38 +162,38 @@ int BitcoinExchange::checkDate(const std::string& date) {
 
     if (size != 3) {
         std::cerr << "Error: invalid date => " << date << std::endl;
-        return -1;
+        return false;
     }
 
     for (int i = 0; i < 3; i++) {
         if (isNumeric(tokens[i], "0123456789") == false) {
             std::cerr << "Error: invalid date => " << date << date << std::endl;
-            return -1;
+            return false;
         }
     }
 
     if (tokens[0].length() != 4 || tokens[1].length() != 2 || tokens[2].length() != 2) {
         std::cerr << "Error: invalid date => " << date << std::endl;
-        return -1;
+        return false;
     }
 
     int tmp;
     std::istringstream(tokens[0]) >> tmp;
     if (tmp < 2009 || tmp > 2022) {
         std::cerr << "Error: invalid date => " << date << std::endl;
-        return -1;
+        return false;
     }
 
     std::istringstream(tokens[1]) >> tmp;
     if (tmp <= 0 || tmp > 12) {
         std::cerr << "Error: invalid date => " << date << std::endl;
-        return -1;
+        return false;
     }
 
     std::istringstream(tokens[2]) >> tmp;
     if (tmp <= 0 || tmp > 31) {
         std::cerr << "Error: invalid date => " << date << std::endl;
-        return -1;
+        return false;
     }
-    return 1;
+    return true;
 }
--- Module_09/ex00/BitcoinExchange.hpp
+++ Module_09/ex00/BitcoinExchange.hpp
@@ -20,8 +20,8 @@ public:
     bool loadDatabase(const std::string& filename);
     double getExchangeRate(const std::string& date) const;
     static std::string trim(const std::string& str);
-    static int checkDate(const std::string& dateStr);
-    static int checkValue(double& value, const std::string& valueStr);
+    static bool checkDate(const std::string& dateStr);
+    static bool checkValue(double& value, const std::string& valueStr);
     static std::pair<bool, std::pair<std::string, double> > parseInput(const std::string& input);
 };
 
